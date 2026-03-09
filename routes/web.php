7c09fd911a3d1074e2042fa01fafef67b7ac54ab
C1l3NCE <anton1108200749@gmail.com>
2026-03-09 15:06:11 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -51,15 +51,6 @@ Route::get('/reports/need-maintenance/pdf', [ReportController::class, 'needMaint
 Route::get('/login', [LoginController::class, 'show'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/reset-all-passwords', function () {
-
-    foreach (User::all() as $user) {
-        $user->password = Hash::make('password');
-        $user->save();
-    }
-
-    return "All passwords reset to: password";
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -226,5 +217,5 @@ Route::middleware(['auth', 'role:driver'])->group(function () {
         '/trips/{trip}',
         [TripController::class, 'destroy']
     )->name('trips.destroy');
-    
+
 });
